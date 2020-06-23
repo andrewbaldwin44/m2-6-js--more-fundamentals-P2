826b832f224f26326c92ccb4cd10cc48423627b0
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-23 11:49:26 -0400

    Solution to exercise 2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -1,5 +1,12 @@
-// Exercise 2
-// ----------
-
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+function createGuestOrder(early, late) {
+  let guests = [...early, ...late];
+
+  guests.forEach((attendee, index) => {
+    console.log(`${index + 1}. ${attendee}`);
+  });
+}
+
+createGuestOrder(earlyBirds, lateComers);
