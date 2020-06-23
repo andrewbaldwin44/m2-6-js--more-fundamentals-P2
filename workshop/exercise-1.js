8a68e0c68f389808bc2bc6df44d80706d66fd06d
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-23 11:43:57 -0400

    Solution to exercise 1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -1,6 +1,3 @@
-// Exercise 1
-// ----------
-
 let mostPopularFood = [
   'poke bowl',
   'chicken sliders',
@@ -12,3 +9,23 @@ let mostPopularFood = [
   'parmesan chicken',
   undefined,
 ];
+
+function fixList(arr, first, last) {
+  let newArr = [...arr];
+
+  newArr.unshift(first);
+  newArr.pop();
+  newArr.push(last);
+
+  return newArr
+}
+
+function countdown(arr) {
+  arr.forEach((food, index) => {
+    console.log(`${index + 1}. ${food}`);
+  });
+}
+
+countdown(
+  fixList(mostPopularFood, 'bean burritos', 'buffalo-flavored cauliflower')
+);
