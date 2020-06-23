1620e0e4e1f3201ade6cf215eef8687c12fad2f3
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-23 12:06:56 -0400

    Solution to exercise 3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -1,6 +1,3 @@
-// Exercise 3
-// ----------
-
 let mostPopularFood = [
   'bean burritos',
   'poke bowl',
@@ -13,3 +10,16 @@ let mostPopularFood = [
   'parmesan chicken',
   'buffalo-flavored cauliflower',
 ];
+
+function sortAlphabetically(list) {
+  return [...list].sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    else return 0;
+  }).forEach(item => {
+    let rank = mostPopularFood.indexOf(item);
+    console.log(`${item} (${rank + 1})`)
+  });
+};
+
+sortAlphabetically(mostPopularFood);
